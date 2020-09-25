2d794ac931b270f28a6ed75b8d0aa27c25bfc74f
Kevin McCullen <kmccullen@users.noreply.github.com>
2020-09-25 11:14:33 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
 bluetooth.startUartService()
 basic.forever(function () {
-    serial.writeValue("a.x", input.acceleration(Dimension.X))
-    serial.writeValue("a.y", input.acceleration(Dimension.Y))
-    serial.writeValue("a.z", input.acceleration(Dimension.Z))
+    bluetooth.uartWriteValue("x", input.acceleration(Dimension.X))
 })
